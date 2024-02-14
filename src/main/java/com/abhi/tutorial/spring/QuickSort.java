3a9056332961ea0ca2718a4ddb138f1d5973f974
Abhi <singhdeveloper937@gmail.com>
2024-02-14 23:00:17 +0530

    Steps.md containing steps for the bean selection by name.

--- src/main/java/com/abhi/tutorial/spring/QuickSort.java
+++ src/main/java/com/abhi/tutorial/spring/QuickSort.java
@@ -1,10 +1,10 @@
 package com.abhi.tutorial.spring;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class QuickSort implements SortAlgoirthm {
 
 	@Override
